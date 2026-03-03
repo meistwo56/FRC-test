91288e42e1a48c25de1a65353aff575abc70df7e
Hao Le <hao.levoquoc2010@gmail.com>
2026-03-03 15:53:01 -0600

    first commit

---
+++ src/main/java/frc/robot/RobotContainer.java
@@ -0,0 +1,23 @@
+package frc.robot;
+
+import frc.robot.Constants.OperatorConstants;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+
+public class RobotContainer {
+
+  private final CommandXboxController m_driverController =
+      new CommandXboxController(OperatorConstants.kDriverControllerPort);
+
+  public RobotContainer() {
+    configureBindings();
+  }
+
+  private void configureBindings() {
+
+  }
+
+  public Command getAutonomousCommand() {
+    return null;
+  }
+}
